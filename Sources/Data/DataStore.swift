b6a6a54c1651f2aada9144e2fa1030bd13574fee
Pedro Almeida <ip4dro@gmail.com>
2025-04-16 00:43:46 +0200

    fix swiftformat action

--- Sources/Data/DataStore.swift
+++ Sources/Data/DataStore.swift
@@ -20,7 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-
 import Models
 
 /// An actor that handles asynchronous storage and management of log entries.
