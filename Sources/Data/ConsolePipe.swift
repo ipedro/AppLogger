17ab9948674b3f3b6850b7a2cf32489a748877a5
Pedro Almeida <ip4dro@gmail.com>
2025-04-27 03:58:15 +0200

    remove unused import

--- Sources/Data/ConsolePipe.swift
+++ Sources/Data/ConsolePipe.swift
@@ -20,7 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-import Combine
 import Foundation
 
 /// Redirects `stdout` and `stderr` into a Combine publisher.
