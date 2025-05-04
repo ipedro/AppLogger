b6b0409c04ca453c4162505d70a159c39f20ad1c
Pedro Almeida <ip4dro@gmail.com>
2025-05-05 01:58:34 +0200

    unify package manifest

--- Package.swift
+++ Package.swift
@@ -1,21 +1,53 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 
 import PackageDescription
+
+/// Information about the current state of the package's git repository.
+let git = Context.gitInformation
+
+/// Whether or not this package is being built for development rather than
+/// distribution as a package dependency.
+let buildingForDevelopment = (git?.currentTag == nil)
+
+func makeList<T>(_ default: T..., development: @autoclosure () -> [T]) -> [T] {
+    if buildingForDevelopment {
+        `default` + development()
+    } else {
+        `default`
+    }
+}
 
 let package = Package(
     name: "swiftui-visual-logger",
     platforms: [
         .iOS(.v15),
     ],
-    products: [
+    products: makeList(
         .library(
             name: "VisualLogger",
             targets: [
                 "VisualLogger",
             ]
         ),
-    ],
-    targets: [
+        development: [
+            .library(
+                name: "VisualLoggerModels",
+                targets: ["Models"]
+            ),
+            .library(
+                name: "VisualLoggerData",
+                targets: ["Data"]
+            ),
+            .library(
+                name: "VisualLoggerUI",
+                targets: ["UI"]
+            ),
+        ]
+    ),
+    dependencies: makeList(development: [
+        .package(url: "https://github.com/swiftlang/swift-testing.git", branch: "6.1.0"),
+    ]),
+    targets: makeList(
         .target(
             name: "Models"
         ),
@@ -25,7 +57,8 @@ let package = Package(
         ),
         .target(
             name: "UI",
-            dependencies: ["Data"]
+            dependencies: ["Data"],
+            swiftSettings: buildingForDevelopment ? [.define("DEBUG_VIEWS")] : []
         ),
         .target(
             name: "VisualLogger",
@@ -35,5 +68,19 @@ let package = Package(
                 "Models",
             ]
         ),
+        development: [
+            .testTarget(
+                name: "DataTests",
+                dependencies: [
+                    "Data",
+                    "Models",
+                    .product(name: "Testing", package: "swift-testing"),
+                ]
+            ),
+        ]
+    ),
+    swiftLanguageModes: [
+        .v5,
+        .v6
     ]
 )
